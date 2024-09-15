deff2a82a5e51994b790bf89e00bb4935e0e80e2
Jaspreet singh <jaspreet.singh70077@gmail.com>
2024-09-15 16:03:07 +0530

    text change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    return res.send("CodeSagar API Latest");
+    return res.send("CodeSagar API");
 });
 
 app.listen(PORT, ()=> {
